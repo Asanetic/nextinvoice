60f5d8e58e7ddc2c4f06ebc79550759709d3d5ba
Asanetic <39214296+Asanetic@users.noreply.github.com>
2025-06-23 08:20:42 +0300

    Initial commit for nextinvoice_v2

--- app/api/apiUtils/s_env/nextinvoice_env.js
+++ app/api/apiUtils/s_env/nextinvoice_env.js
@@ -8,8 +8,8 @@ const mosyDbConfig = {
   },
   production: {
     DB_HOST: '127.0.0.1',
-    DB_USER: 'firebirdbill',
-    DB_PASS: 'firebirdbill001',
+    DB_USER: 'nextadmin',
+    DB_PASS: 'UltraSecurePass123!',
     DB_NAME: 'firebirdbill',
   }
 };
