1fa3dce4239de59c940079d09324cd868241a2aa
Asanetic <39214296+Asanetic@users.noreply.github.com>
2025-06-29 08:05:41 +0300

    Initial commit for  nextinvoice_v4

--- app/page.js
+++ app/page.js
@@ -1,4 +1,10 @@
+import AuthPage from "./auth/login/page";
+
   
   export default function Home() {
-    return <h1>Welcome to nextinvoice 🚀</h1>;
+   return(
+    <>
+    <AuthPage/>
+    </>
+   )
   }
